a15ded18604dad76dc2f8a5153d56698535bb966
danbiilee <danbi.db@gmail.com>
2021-04-30 08:54:28 +0900

    #16 [SMS] ServerGroup 컴포넌트 렌더

--- src/pages/sms/developContainer.js
+++ src/pages/sms/developContainer.js
@@ -1,13 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import CenterName from "../../components/centerName/index";
+import ServerGroup from "../../components/server/ServerGroup";
 
 const developContainer = ({ style }) => {
   const Data = [
     {
       centerName: "개발/테스트",
+      groups: [
+        {
+          id: 1,
+          servers: [
+            { id: 1, status: 0, serverName: "SERVER 107" },
+            { id: 2, status: 0, serverName: "SERVER 107" },
+            { id: 3, status: 0, serverName: "SERVER 107" },
+          ],
+        },
+      ],
+      statuses: [
+        { id: 1, status: 0, value: 39 },
+        { id: 2, status: 1, value: 142 },
+        { id: 3, status: 2, value: 5 },
+      ],
       style: {
-        width: "215px",
+        width: "214px",
         height: "50px",
         top: "11px",
         left: "7px",
@@ -16,8 +32,23 @@ const developContainer = ({ style }) => {
     },
     {
       centerName: "본사",
+      groups: [
+        {
+          id: 1,
+          servers: [
+            { id: 1, status: 0, serverName: "SERVER 107" },
+            { id: 2, status: 0, serverName: "SERVER 107" },
+            { id: 3, status: 0, serverName: "SERVER 107" },
+          ],
+        },
+      ],
+      statuses: [
+        { id: 1, status: 0, value: 39 },
+        { id: 2, status: 1, value: 142 },
+        { id: 3, status: 2, value: 5 },
+      ],
       style: {
-        width: "215px",
+        width: "214px",
         height: "50px",
         top: "676px",
         left: "7px",
@@ -28,8 +59,12 @@ const developContainer = ({ style }) => {
 
   return (
     <section className="p-abs" style={style}>
-      <CenterName data={Data[0]} style={Data[0].style} />
-      <CenterName data={Data[1]} style={Data[1].style} />
+      {Data.map((center, index) => (
+        <div key={index}>
+          <CenterName data={center} style={center.style} />
+          <ServerGroup data={center} index={index} />
+        </div>
+      ))}
     </section>
   );
 };
