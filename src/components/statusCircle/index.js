129ebd9a833ba4354016076fb946d7d6d09b085d
danbiilee <danbi.db@gmail.com>
2021-04-30 10:46:13 +0900

    #9 [COMMON] StatusCircle: theme color 적용

--- src/components/statusCircle/index.js
+++ src/components/statusCircle/index.js
@@ -7,20 +7,11 @@ const Wrapper = styled.span`
   width: ${(props) => (props.size === "lg" ? 14 : 12)}px;
   height: ${(props) => (props.size === "lg" ? 14 : 12)}px;
   border-radius: 50%;
-  background-color: ${(props) => props.color};
+  background-color: ${(props) => props.theme.color.statusColor[props.status]};
 `;
 
-const StatusCircle = ({ data, size }) => {
-  // 컬러값 global config에 담아서 사용하기
-  const colorCode = {
-    0: "#d43f3a", // red
-    1: "#ff8417", // orange
-    2: "#8c9497", // gray
-    3: "#99c165", // green
-  };
-  const color = colorCode[data.status];
-
-  return <Wrapper color={color} size={size} />;
+const StatusCircle = ({ data: { status }, size }) => {
+  return <Wrapper status={status} size={size} />;
 };
 
 StatusCircle.propTypes = {
