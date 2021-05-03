58200dddefa52a348dfa4f6a90f78b658b9b9056
Dongwook Kim <dongwookkim@nkia.co.kr>
2021-05-03 09:20:21 +0900

    #7 헤더 색상변경 추가

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,14 +3,16 @@ import { createGlobalStyle } from "styled-components";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { clickTab } from "../../redux/uiSlice";
+import theme from "../../helpers/theme";
 
 const GlobalStyle = createGlobalStyle`
   .header {
     color: #425363;
     font-size: 18px;
     height: 56px;
-    border-bottom: 2px solid #425363;
+    border-bottom: ${(props) => props.theme.borderBottom};
     z-index: 999; // 이미지 없애면 삭제 할 것
+    background-color: ${(props) => props.theme.background}
   }
 
   .tab {
@@ -20,29 +22,21 @@ const GlobalStyle = createGlobalStyle`
     align-items: center;
 
     &__item {
-      border: 1px solid white;
       cursor: pointer;
-      
     }
     &__link {
       padding: 17px 33px;
       display: block;
-      color: #333d4b;
-      background-color: #425363;
+      background-color: ${(props) => props.theme.background};
+      color: ${(props) => props.theme.inactiveColor};
       text-decoration: none;
       text-transform: uppercase;
 
       &.active {
-        color: #fff;
+        color: ${(props) => props.theme.activeColor};
+        background-color: ${(props) => props.theme.activeBackground};
         pointer-events: none;
-      }
-      &--1.active {
-        border-top: 3px solid #79ab3d;
-        background-color: red;
-      }
-      &--2.active {
-        border-top: 3px solid #67faff;
-        background-color: #0a0b0d;
+        border-top: ${(props) => props.theme.activeBorderTop}
       }
     }
   }
@@ -53,7 +47,6 @@ const Header = () => {
   const currentTab = useSelector((state) => state.ui.tab);
 
   const handleClickTab = (e) => {
-    e.preventDefault();
     const clicked = e.currentTarget.getAttribute("data-value");
     if (currentTab === clicked) return;
     dispatch(clickTab(clicked));
@@ -61,7 +54,7 @@ const Header = () => {
 
   return (
     <>
-      <GlobalStyle />
+      <GlobalStyle theme={theme.color.header[currentTab]} />
       <header className="header">
         <ul className="tab">
           <li className="tab__item" data-value={SMS} onClick={handleClickTab}>
--- src/helpers/theme/color.js
+++ src/helpers/theme/color.js
@@ -1,10 +1,27 @@
 const color = {
-  // 상태별 컬러
   statusColor: {
     0: "#d43f3a", // red
     1: "#ff8417", // orange
     2: "#8c9497", // gray
     3: "#99c165", // green
+  },
+  header: {
+    sms: {
+      activeBackground: "#425363",
+      activeBorderTop: "5px solid #79ab3d",
+      activeColor: "#fff",
+      background: "#fff",
+      inactiveColor: "#333d4b",
+      borderBottom: "2px solid #425363",
+    },
+    nms: {
+      activeBackground: "#000",
+      activeBorderTop: "5px solid #67faff",
+      activeColor: "#fff",
+      background: "#292b31",
+      inactiveColor: "#666c7e",
+      borderBottom: "0",
+    },
   },
 };
 
