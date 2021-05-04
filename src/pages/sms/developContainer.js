02f6e5451785b34d6e0a5bbf0a1c4f7a6989c48f
danbiilee <danbi.db@gmail.com>
2021-05-04 13:45:30 +0900

    #22 [DATA] SMS 페이지 리덕스 연결

--- src/pages/sms/developContainer.js
+++ src/pages/sms/developContainer.js
@@ -1,68 +1,41 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
 import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 import CenterName from "../../components/centerName/index";
 import ServerGroup from "../../components/server/ServerGroup";
 
 const developContainer = ({ style }) => {
-  const Data = [
+  const centerStyle = {
+    width: "214px",
+    height: "50px",
+    position: "absolute",
+    left: "7px",
+  };
+  const { list: sidetopList } = useSelector((state) => state.sms.sidetop);
+  const { list: sidebottomList } = useSelector((state) => state.sms.sidebottom);
+
+  const data = [
     {
-      centerName: "개발/테스트",
-      groups: [
-        {
-          id: 1,
-          servers: [
-            { id: 1, status: 0, serverName: "SERVER 107" },
-            { id: 2, status: 0, serverName: "SERVER 107" },
-            { id: 3, status: 0, serverName: "SERVER 107" },
-          ],
-        },
-      ],
-      statuses: [
-        { id: 1, status: 0, value: 39 },
-        { id: 2, status: 1, value: 142 },
-        { id: 3, status: 2, value: 5 },
-      ],
-      style: {
-        width: "214px",
-        height: "50px",
-        top: "11px",
-        left: "7px",
-        position: "absolute",
-      },
+      type: "sidetop",
+      centerName: GLOBAL_CONFIG.sidetop.centerName,
+      list: sidetopList,
+      style: { ...centerStyle, top: "11px" },
     },
     {
-      centerName: "본사",
-      groups: [
-        {
-          id: 1,
-          servers: [
-            { id: 1, status: 0, serverName: "SERVER 107" },
-            { id: 2, status: 0, serverName: "SERVER 107" },
-            { id: 3, status: 0, serverName: "SERVER 107" },
-          ],
-        },
-      ],
-      statuses: [
-        { id: 1, status: 0, value: 39 },
-        { id: 2, status: 1, value: 142 },
-        { id: 3, status: 2, value: 5 },
-      ],
-      style: {
-        width: "214px",
-        height: "50px",
-        top: "676px",
-        left: "7px",
-        position: "absolute",
-      },
+      type: "sidebottom",
+      centerName: GLOBAL_CONFIG.sidebottom.centerName,
+      list: sidebottomList,
+      style: { ...centerStyle, top: "676px" },
     },
   ];
 
   return (
     <section className="p-abs" style={style}>
-      {Data.map((center, index) => (
+      {data.map((item, index) => (
         <div key={index}>
-          <CenterName data={center} style={center.style} />
-          <ServerGroup data={center} index={index} />
+          <CenterName centerName={item.centerName} style={item.style} />
+          <ServerGroup type={item.type} list={item.list} />
         </div>
       ))}
     </section>
@@ -72,4 +45,5 @@ const developContainer = ({ style }) => {
 developContainer.propTypes = {
   style: PropTypes.object,
 };
+
 export default developContainer;
